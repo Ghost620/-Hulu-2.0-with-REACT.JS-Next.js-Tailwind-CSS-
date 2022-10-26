41ce6df357e8d6b44989124acb031bd6b370ee14
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-27 01:56:20 +0500

    nav added

--- hulu/components/Header.js
+++ hulu/components/Header.js
@@ -1,16 +1,21 @@
 import Image from "next/image"
 import HeaderItem from "./HeaderItem"
-import {BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon} from "@heroicons/react/24/outline"
+import { CheckBadgeIcon, CircleStackIcon, HomeIcon, BoltIcon, MagnifyingGlassIcon, UserIcon } from "@heroicons/react/24/outline"
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
 
-        <div>
+        <div className="flex flex-grow justify-evenly max-w-2xl">
             <HeaderItem title='HOME' Icon={HomeIcon}/>
+            <HeaderItem title='TRENDING' Icon={BoltIcon}/>
+            <HeaderItem title='VERIFIED' Icon={CheckBadgeIcon}/>
+            <HeaderItem title='COLLECTIONS' Icon={CircleStackIcon}/>
+            <HeaderItem title='SEARCH' Icon={MagnifyingGlassIcon}/>
+            <HeaderItem title='ACCOUNT' Icon={UserIcon}/>
         </div>
 
-        <img className="object-contain" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Hulu_Logo.svg/220px-Hulu_Logo.svg.png' width={200} height={200} />
+        <img className="object-contain pt-5 sm:pt-0" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Hulu_Logo.svg/220px-Hulu_Logo.svg.png' width={200} height={200} />
 
 
     </header>
