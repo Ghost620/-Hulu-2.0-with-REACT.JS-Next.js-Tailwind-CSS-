ad95c73b5ff4e229cec6ea8d9a3fa6973dfd7dd9
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-28 22:59:50 +0500

    done

--- hulu/components/Results.js
+++ hulu/components/Results.js
@@ -1,15 +1,15 @@
 import Thumbnail from "./Thumbnail"
+import FlipMove from "react-flip-move"
 
 const Results = ( { results })  => {
-    console.log(results)
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 3xl:flex flex-wrap justify-center">
         {
             results.map(res => (
                 <Thumbnail key={res.id} result={res}/>
             ))
         }
-    </div>
+    </FlipMove>
   )
 }
 
